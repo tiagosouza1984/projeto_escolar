ff2f449fec0b7581ffefe5cc7899d2d63ed4e776
MAC <mac@MacBook-Air-6.local>
2019-05-26 07:44:09 -0300

    update forca e menu finalizados

--- python/jogo.py
+++ python/jogo.py
@@ -1,6 +1,7 @@
 ﻿
 import forca
-print(__name__)
+print("estou no jogo")
+
 
 def menu():
     print('     Menu\n')
@@ -9,44 +10,33 @@ def menu():
     print('Digite 2 para jogo da velha')
     print('Digite 3 para sair\n')    
 
-menu()
-def main():
-    flag = False
-    while not flag:
-        close = False
-        opcao = input('Sua opção: ')
-        if opcao == '1':      
-            print('Jogo da Forca')
-            forca.main()
-            flag = True
-        elif opcao == '2':
-            print('Jogo da Velha')   
-            flag = True   
-        elif opcao == '3':
-            while not close:
-                resp = input('Tem certeza que deseja sair?\nDigite "Sim" para sair ou "Não" para continuar: ')
-                resp = resp.upper()              
-                if resp == 'SIM':
-                    print('Até a próxima!')
-                    flag = True
-                    close = True
-                elif resp == 'NÃO':
-                    close = True
-                    print('\n')
-                else:
-                    print('Opção não identificada!\n')
-        else:
-            print('Opção não identificada!\n')
-if __name__ == "__main__":
-    main()
+
+flag = False
+while not flag:
+    close = False
+    menu()
+    opcao = input('Sua opção: ')
+    if opcao == '1':      
+        print('Jogo da Forca')
+        forca.main()
+        flag = False
+    elif opcao == '2':
+        print('Jogo da Velha')   
+        flag = True   
+    elif opcao == '3':
+        while not close:
+            resp = input('Tem certeza que deseja sair?\nDigite "Sim" para sair ou "Não" para continuar: ')
+            resp = resp.upper()              
+            if resp == 'SIM':
+                print('Até a próxima!')
+                flag = True
+                close = True
+            elif resp == 'NÃO':
+                close = True
+                print('\n')
+            else:
+                print('Opção não identificada!\n')
+    else:
+        print('Opção não identificada!\n')
 
 
-"""
-import forca, velha
-def main():
-    <bloco de código para perguntar em um laço qual o jogo, com menu, opções para escolher o jogo, sair, etc.>
-        <se jogo da velha, chamar --> velha.main()>
-        <se jogo da forca, chamar --> forca.main()>
-if __name__ == ‘__main__’:
-    main()
-"""
